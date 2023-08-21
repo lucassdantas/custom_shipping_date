e41544f11e15b96ca6442920e6c36b34573a2641
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-08-21 15:06:14 -0300

    first commit

---
+++ custom_shipping_date.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: Calculadora Bruno
+* Plugin URI: https://github.com/lucassdantas/calculadora-bruno-investimentos
+* Description: Calculadora para o site do Buno Investimentos
+* Version: 0.7
+* Author: R&D Marketing Digital
+* Author URI: https://rdmarketing.com.br/
+**/
