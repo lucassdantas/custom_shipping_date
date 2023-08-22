0c64cf47a3653b6d44c6687642002a730c2271ea
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-08-22 10:36:49 -0300

    buildign script

--- custom_shipping_date.php
+++ custom_shipping_date.php
@@ -7,3 +7,24 @@
 * Author: R&D Marketing Digital
 * Author URI: https://rdmarketing.com.br/
 **/
+
+defined('ABSPATH') or die();
+if(!function_exists('add_action')){
+    die;
+}
+
+function script_custom_date()
+{
+    ?>
+    <script>
+        let ds = value => document.querySelector(value),
+            c  = value => console.log(value),
+            deliveryTypeSelect =  ds("#billing_delivery_type"),
+            deliveryDateField = ds("#") 
+
+    </script>
+    
+    <?php
+}
+  
+    add_action('wp_footer', 'costScript');
