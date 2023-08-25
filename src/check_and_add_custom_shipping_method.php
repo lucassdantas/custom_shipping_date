254fb59493935abf9cae2f1177176c6fa6089ccf
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-08-25 16:58:17 -0300

    need to fix relaod error

--- src/check_and_add_custom_shipping_method.php
+++ src/check_and_add_custom_shipping_method.php
@@ -1,5 +1,5 @@
 <?php
-add_action('woocommerce_before_checkout_billing_form', 'check_and_add_custom_shipping_method');
+add_action('woocommerce_before_checkout_form', 'check_and_add_custom_shipping_method');
 function check_and_add_custom_shipping_method() {
 	$current_shipping_name = "";
 	$current_shipping_method = WC()->session->get( 'chosen_shipping_methods' );
@@ -12,6 +12,46 @@ function check_and_add_custom_shipping_method() {
 		}
 	}
 	if($current_shipping_name === 'Entrega por Motoboy'){
-		require_once plugin_dir_path( __FILE__ ) . 'src/custom_date_field.php';
+		add_filter('woocommerce_checkout_fields', 'custom_date_field');
+		function custom_date_field($fields)
+		{
+			$fields['billing']['shipping_type'] = array(
+				'label'     => __('Tipo de entrega', 'woocommerce'),
+				'type'		=> 'select',
+				'placeholder'   => _x('Selecione o tipo de entrega', 'placeholder', 'woocommerce'),
+				'required'  => true,
+				'class'     => array('form-row-wide'),
+				'clear'     => true
+			);
+			
+			$fields['billing']['shipping_type']['options'] = array (
+				'Entrega imediata' => 'Entrega imediata',
+				'Próximo dia útil' => 'Próximo dia útil',
+				'Entrega agendada' => 'Agendar entrega'
+			);
+			
+			$fields['billing']['shipping_type']['priority'] = 8;
+			
+			
+			$fields['billing']['shipping_date'] = array(
+				'type' => 'date',
+				'label'     => __('Data de entrega', 'woocommerce'),
+				'placeholder'   => _x('Data de entrega', 'placeholder', 'woocommerce'),
+				'required'  => false,
+				'class'     => array('form-row-wide'),
+				'clear'     => true
+			);
+
+			$fields['billing']['shipping_date']['priority'] = 9;
+			return $fields;
+		}
+
+		add_action( 'woocommerce_admin_order_data_after_shipping_address', 'display_shipping_type_on_order', 10, 1 );
+
+		function display_shipping_type_on_order($order){
+			echo '<p><strong>'.__('Tipo de entrega:').'</strong> ' . get_post_meta( $order->get_id(), '_shipping_type', true ) . '</p>';
+			echo '<p><strong>'.__('Data:').'</strong> ' . get_post_meta( $order->get_id(), '_shipping_date', true ) . '</p>';
+		}
+
 	}
 }
--- src/script_custom_date.php
+++ src/script_custom_date.php
@@ -30,35 +30,64 @@ function script_custom_date()
             date;
 
         waitForElementToExist('#shipping_type')
-            .then( shippingType => {
-                waitForElementToExist('#shipping_date_field')
-                    .then(shippingDateField => {
-                        let showShippingDateByShippingType = (shippingType, shippingDate) => {
-                            if(shippingType.selectedIndex !== 2){
-                                shippingDate.style.display = 'none'
-                            }else{
-                                shippingDate.style.display = ''
-                            }
-                        }
-                        showShippingDateByShippingType(shippingType, shippingDateField)
-                        shippingDateField.querySelector('.optional').innerHTML = ''
-                        let shippingDateInput = shippingDateField.querySelector('input')
-                        
-                        shippingType.addEventListener('change', e => {
-                            showShippingDateByShippingType(shippingType, shippingDateField)
-                        })
-                        shippingDateInput.addEventListener('change', e => {
-                            date = new Date(shippingDateInput.valueAsNumber)
-                            if(date.getUTCDay() == 0){
-                                shippingDateInput.value = ''
-                                alert('Só realizamos entregas em dias úteis')
-                            }
-                        })
-                    })
+        .then( shippingType => {
+            waitForElementToExist('#shipping_date_field')
+            .then(shippingDateField => {
+                let showShippingDateByShippingType = (shippingType, shippingDate) => {
+                    if(shippingType.selectedIndex !== 2){
+                        shippingDate.style.display = 'none'
+                    }else{
+                        shippingDate.style.display = ''
+                    }
+                }
+                showShippingDateByShippingType(shippingType, shippingDateField)
+                shippingDateField.querySelector('.optional').innerHTML = ''
+                let shippingDateInput = shippingDateField.querySelector('input')
+                
+                shippingType.addEventListener('change', e => {
+                    showShippingDateByShippingType(shippingType, shippingDateField)
                 })
+                shippingDateInput.addEventListener('change', e => {
+                    date = new Date(shippingDateInput.valueAsNumber)
+                    if(date.getUTCDay() == 0){
+                        shippingDateInput.value = ''
+                        alert('Só realizamos entregas em dias úteis')
+                    }
+                })
+            })
+        })
     }
 </script>
+<script>
+    if(location.pathname === '/finalizar-compra/'){
+        let setCheckListener = (element) => {
+            if(element.checked === true){
+                console.log('checked = true')
+                let tryReloadPage = id => {
+                    let element = document.querySelector("#"+id)
+                    if(!element.checked){ 
+                        location.reload()
+                        return true
+                    }
+                    return false
+                },
+                clearReloadInterval = (interval) => {
+                    if(tryReloadPage('shipping_method_0_free_shipping6')) clearInterval(interval)
+                },
+                reloadPageInterval = setInterval(tryReloadPage, 1200, 'shipping_method_0_free_shipping6' );
+                setInterval(clearReloadInterval, 500, reloadPageInterval)
+            }
+        },
+        motoboyShipping = document.querySelector("#shipping_method_0_free_shipping6")
+        
+        motoboyShipping.addEventListener('click', (e) => {
+            e.preventDefault()
+            location.reload();
+        })
 
+        setCheckListener(motoboyShipping)
+    }
+</script>
 <?php
 }
 
