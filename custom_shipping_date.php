960edaed7e5c05688d0a43996e4bb9e643f32ab0
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-09-05 15:15:17 -0300

    .

--- custom_shipping_date.php
+++ custom_shipping_date.php
@@ -3,7 +3,7 @@
 * Plugin Name: Custom Shipping Date
 * Plugin URI: https://github.com/lucassdantas/custom_shipping_date
 * Description: Custom date for woocommerce shipping
-* Version: 0.3
+* Version: 0.5
 * Author: R&D Marketing Digital 
 * Author URI: https://rdmarketing.com.br/
 **/
@@ -12,6 +12,7 @@ defined('ABSPATH') or die();
 if(!function_exists('add_action')){
     die;
 }
+require_once plugin_dir_path(__FILE__). 'src/restrict_motoboy_shipping.php';
 require_once plugin_dir_path( __FILE__ ). 'src/check_and_add_custom_shipping_method.php';
 require_once plugin_dir_path( __FILE__ ). 'src/script_custom_date.php';
 
---
+++ src/restrict_motoboy_shipping.php
@@ -0,0 +1,27 @@
+<?php 
+
+// Adicione o filtro para modificar as opções de entrega
+add_filter('woocommerce_package_rates', 'custom_shipping_methods_based_on_categories', 10, 2);
+
+function custom_shipping_methods_based_on_categories($rates, $package) {
+    // Verifique se a entrega por motoboy está disponível
+    $motoboy_available = true;
+
+    // Verificamos se algum produto não pertence às categorias "baterias de carro" ou "baterias de moto"
+    foreach (WC()->cart->get_cart() as $cart_item) {
+        $product_id = $cart_item['product_id'];
+        $term_names = wp_get_post_terms($product_id, 'product_cat', array('fields' => 'names'));
+
+        if (!in_array('Baterias de Carro', $term_names) && !in_array('Baterias de Moto', $term_names)) {
+            $motoboy_available = false;
+            break;
+        }
+    }
+
+    // Desative a entrega por motoboy se a condição não for atendida
+    if (!$motoboy_available) {
+        unset($rates['entrega_por_motoboy']);
+    }
+
+    return $rates;
+}
