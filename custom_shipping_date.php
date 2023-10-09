e40780b0617ebfe3544109ff572fa5fa3ce74a06
Lucas Dantas <lucasdantasprogramador@gmail.com>
2023-10-09 14:56:24 -0300

    removing help baterias shipping method based on product quantity

--- custom_shipping_date.php
+++ custom_shipping_date.php
@@ -13,7 +13,9 @@ if(!function_exists('add_action')){
     die;
 }
 
+global $product_quantity;
 require_once plugin_dir_path(__FILE__). 'src/add_motoboy_shipping_method.php';
+require_once plugin_dir_path(__FILE__). 'src/restrict_help_baterias_shipping_on_cart.php';
 require_once plugin_dir_path(__FILE__). 'src/restrict_motoboy_shipping.php';
 require_once plugin_dir_path( __FILE__ ). 'src/check_and_add_custom_shipping_method.php';
 require_once plugin_dir_path( __FILE__ ). 'src/script_custom_date.php';
--- src/restrict_help_baterias_shipping_on_cart.php
+++ src/restrict_help_baterias_shipping_on_cart.php
@@ -5,13 +5,27 @@ defined('ABSPATH') or die();
 if(!function_exists('add_action')){
     die;
 }
-
 function restrict_help_baterias_shipping_on_cart($cart) {
+    $count = 0;
     foreach($cart->get_cart() as $cart_item){
+        $single_product_quantity = $cart_item['quantity'];
         echo "<pre>";
         print_r($cart_item);
         echo "</pre>";
-        
+        if($single_product_quantity > 1){
+            $count += ($single_product_quantity - 1);
+        }  
+        $count++;
+        echo "<p id='plugin_test'>$count</p>";
+    }
+    if($count > 6){
+        add_filter( 'woocommerce_package_rates', 'disable_shipping_method_based_on_postcode', 10, 2 );
+        function disable_shipping_method_based_on_postcode( $rates, $package ) {
+            foreach ( $rates as $rate_id => $rate ) {
+                unset( $rates['help_baterias_shipping34'] );
+            }
+            return $rates;
+        }
     }
 }
 add_action('woocommerce_before_calculate_totals', 'restrict_help_baterias_shipping_on_cart');
