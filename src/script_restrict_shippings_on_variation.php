1e106184b150f16abfe6da185a4edbbdf221b21d
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-07-04 14:04:04 -0300

    change restrict script on single product page

--- src/script_restrict_shippings_on_variation.php
+++ src/script_restrict_shippings_on_variation.php
@@ -14,7 +14,6 @@ function script_restrict_shippings_on_variation()
             cepInput = document.querySelector("#inputCep");
         
         if (!variationSelector || !cepInput) {
-            console.log('returned')
             return;
         }
 
@@ -27,13 +26,23 @@ function script_restrict_shippings_on_variation()
                 const intervalId = setInterval(() => {
                     let rows = document.querySelectorAll('.resultado-frete tr');
                     if (rows.length > 0) {
+                        let messageAdded = false;  
                         rows.forEach(tr => {
                             let td = tr.querySelector('td');
                             if (td && td.innerText !== 'Entrega Help Baterias : R$0,00') {
                                 tr.style.display = 'none';
+                                if (!messageAdded) {
+                                    let messageRow = document.createElement('tr');
+                                    let messageTd = document.createElement('td');
+                                    messageTd.colSpan = td.colSpan;  
+                                    messageTd.innerText = 'As entregas por transportadora não estão disponíveis para produtos com devolução de sucata.';
+                                    messageRow.appendChild(messageTd);
+                                    tr.parentNode.insertBefore(messageRow, tr.nextSibling);
+                                    messageAdded = true;
+                                }
                             }
                         });
-                        clearInterval(intervalId); 
+                        clearInterval(intervalId);
                     }
                 }, 100);  
             }
