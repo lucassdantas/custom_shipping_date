802ad1e60eaa9eef7b62bcd9a6bb22e6624557a3
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-07-04 10:18:32 -0300

    restrict shipping by waste devolution variation

--- src/restrict_help_baterias_shipping_on_cart.php
+++ src/restrict_help_baterias_shipping_on_cart.php
@@ -1,35 +1,38 @@
 <?php 
 
-
 defined('ABSPATH') or die();
 if(!function_exists('add_action')){
     die;
 }
+
 function restrict_help_baterias_shipping_on_cart($cart) {
-    $count = 0;
+    $remove_other_shipping_methods = false;
+
     foreach($cart->get_cart() as $cart_item){
-        $single_product_quantity = $cart_item['quantity'];
-        if(in_array('com-sucata', $cart_item['variation'])){
-            echo "O produto possui sucata";
-        }
-
-        if($single_product_quantity > 1){
-            $count += ($single_product_quantity - 1);
-        }  
-        $count++;
-    }
-
-    /*
-    #Removed because the limit dont exit anymore
-    if($count > 6){
-        add_filter( 'woocommerce_package_rates', 'disable_shipping_method_based_on_postcode', 10, 2 );
-        function disable_shipping_method_based_on_postcode( $rates, $package ) {
-            foreach ( $rates as $rate_id => $rate ) {
-                unset( $rates['help_baterias_shipping34'] );
-            }
-            return $rates;
+        if(in_array('sem-sucata', $cart_item['variation'])){
+            $remove_other_shipping_methods = true;
+            break;
         }
     }
-    */
+
+    if($remove_other_shipping_methods){
+        require_once plugin_dir_path(__FILE__). './restrict_shipping_by_product_variation.php';
+    }
 }
+
 add_action('woocommerce_before_calculate_totals', 'restrict_help_baterias_shipping_on_cart');
+
+
+
+/*
+#Removed because the limit dont exit anymore
+if($count > 6){
+    add_filter( 'woocommerce_package_rates', 'disable_shipping_method_based_on_postcode', 10, 2 );
+    function disable_shipping_method_based_on_postcode( $rates, $package ) {
+        foreach ( $rates as $rate_id => $rate ) {
+            unset( $rates['help_baterias_shipping34'] );
+        }
+        return $rates;
+    }
+}
+*/
